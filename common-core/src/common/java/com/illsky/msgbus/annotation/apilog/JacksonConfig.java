f6768b6ae1558965699510fc32adc2650d474c4d
sucongcong <sucongcong@haier.com>
2025-09-10 15:20:43 +0800

    feat(common-core): 添加 Jackson 配置类
    - 新增 JacksonConfig 类，配置 ObjectMapper 以支持 Java Time 模块
    - 添加 LocalDateTime 和 LocalDate 的序列化器，使用指定的日期时间格式
    -禁用将日期作为时间戳写入的功能

---
+++ common-core/src/common/java/com/illsky/msgbus/annotation/apilog/JacksonConfig.java
@@ -0,0 +1,32 @@
+package com.illsky.msgbus.annotation.apilog;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author sucongcong
+ */
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        objectMapper.registerModule(javaTimeModule);
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
+    }
+}
